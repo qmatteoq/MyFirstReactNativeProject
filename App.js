1297380e3649dcda21e1222e1774110b606c49ad
Matteo Pagani <matteo.pagani@microsoft.com>
2020-02-05 19:46:01 +0100

    Added starting point

--- App.js
+++ App.js
@@ -24,10 +24,6 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import HelloComponent from './components/helloComponent'
-import HelloComponentWithState from './components/helloComponentWithState'
-import HelloComponentWithHooks from './components/helloComponentWithHooks'
-
 const App = () => {
   return (
     <Fragment>
@@ -42,9 +38,34 @@ const App = () => {
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
-         <HelloComponent message="world!" name="Matteo" />
-         <HelloComponentWithState message="world!" name="Matteo" />
-         <HelloComponentWithHooks message="world!" name="Matteo" />
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>This is my hello world!</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
         </ScrollView>
       </SafeAreaView>
     </Fragment>
